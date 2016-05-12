9b870ff6b772b2c41f52e9465a5f8a6c1cc92c94
Graeme Mathieson <mathie@woss.name>
2016-05-12 08:48:11 +0100

    Get the xml datetime right.

--- config.rb
+++ config.rb
@@ -96,7 +96,7 @@ helpers do
 
   def xml_timestamp(dateish = Time.now)
     timestamp = dateish.respond_to?(:strftime) ? dateish : Date.parse(dateish)
-    timestamp.strftime('%FT%H:%M:%S%z') 
+    timestamp.strftime('%FT%H:%M:%S%:z') 
   end
 end
 
