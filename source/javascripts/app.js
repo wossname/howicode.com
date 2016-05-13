ae1d92fd8c30fde28bfbf71a2600ca02232c72dd
Graeme Mathieson <mathie@woss.name>
2016-05-13 09:10:26 +0100

    Nuke the almost empty JS file.

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -319,3 +319,16 @@ var App = function () {
 		},
 	};
 }();
+
+jQuery(document).ready(function() {
+  App.init();
+});
+
+$.backstretch([
+  "/images/14.jpg",
+  "/images/17.jpg",
+  "/images/2.jpg"
+], {
+  fade: 1000,
+  duration: 7000
+});
